92b6b7022e41cda7cce100190cc95e3fd17fc775
Wachira Beatrice <BeatriceWambui>
2019-10-05 20:50:13 +0300

    import redirect and add definations for personal_photos and hobby_photos

--- picha/views.py
+++ picha/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # import datetime as dt
 # Create your views here.
 
 def welcome(request):
     return render (request,'index.html')
+
+def personal_photos(request):
+    return render (request,'all-photos/personal-photos.html')
+
+def hobby_photos(request):
+    return render (request,'all-photos/hobby-photos.html')
