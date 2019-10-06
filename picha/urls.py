851a1f1445367c2489343edfeec1434eb91426b4
Wachira Beatrice <BeatriceWambui>
2019-10-06 16:16:10 +0300

    create a url for search create a url for search

--- picha/urls.py
+++ picha/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
 urlpatterns=[
     url(r'^$',views.personal_photos,name ='myPhotos'),
     url(r'^$',views.hobby_photos,name='hobbyPhotos'),
+    url(r'^search/',views.search-results,name='search_results')
 ]
+
+if settings.Debug:
+    urlpatterns+= static(settings.MEDIA_url,document_root = settings.MEDIA_ROOT)
