3a3bcd4442abfce6897a6daef7b7df549e134535
Wachira Beatrice <BeatriceWambui>
2019-10-06 21:19:21 +0300

    correct urls.py

--- picha/urls.py
+++ picha/urls.py
@@ -6,8 +6,9 @@ from . import views
 urlpatterns=[
     url(r'^$',views.personal_photos,name ='myPhotos'),
     url(r'^$',views.hobby_photos,name='hobbyPhotos'),
-    url(r'^search/',views.search-results,name='search_results')
+    # url(r'^search/',views.search-results,name='search_results'),
+    # url(r'^location/(\d+)',views.location,name='picha'),
 ]
 
-if settings.Debug:
-    urlpatterns+= static(settings.MEDIA_url,document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
