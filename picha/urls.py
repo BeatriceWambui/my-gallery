bae19295c32495bcb860124669b30c51ce17f128
Wachira Beatrice <BeatriceWambui>
2019-10-07 00:45:26 +0300

    remove personal photos and hobby photos url and create a search url

--- picha/urls.py
+++ picha/urls.py
@@ -4,9 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.personal_photos,name ='myPhotos'),
-    url(r'^$',views.hobby_photos,name='hobbyPhotos'),
-    # url(r'^search/',views.search-results,name='search_results'),
+    url(r'^$',views.index,name ='myPhotos'),
+    url(r'^search/',views.search_results,name='search_results'),
     # url(r'^location/(\d+)',views.location,name='picha'),
 ]
 
