8c85f8e0ab711139b964fcb50c4feb1e29ea90dd
Wachira Beatrice <BeatriceWambui>
2019-10-06 16:16:52 +0300

    create a def for search results of the images

--- picha/views.py
+++ picha/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Image,Location
 # import datetime as dt
 # Create your views here.
 
@@ -11,3 +11,16 @@ def personal_photos(request):
 
 def hobby_photos(request):
     return render (request,'all-photos/hobby-photos.html')
+
+def search_results(request):
+    if 'image'in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        searched_images = Image.searched_by_category(search_term)
+
+        message = f'{search_term}'
+
+        return render(request, 'all-photos/search.html',{'message':message,'images':searched_images})
+
+    else:
+        message = 'You havent searched for any image'
+        return render(request,'all-images/search.html',{'message':message})
