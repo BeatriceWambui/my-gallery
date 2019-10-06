aa872102e9952f58e3a87798da3f37cd921593be
Wachira Beatrice <BeatriceWambui>
2019-10-06 11:45:00 +0300

    create a class for location and category

--- picha/models.py
+++ picha/models.py
@@ -2,19 +2,28 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    first_name = models.CharField(max_length =40)
-    last_name = models.CharField(max_length =40)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.first_name
-    class Meta:
-        ordering = ['first_name']
 
 class Category(models.Model):
     name = models.CharField(max_length = 40)
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# class Image(models.Model):
+#     image = models.ImageField(upload_to='images/')
+#     image_name = models.CharField(max_length = 40)
+#     image_description = models.TextField()
+#     user = models.ForeignKey(User)
+#     category = models.ForeignKey(User)
+#     location = models.ForeignKey(Location)
+
+#     def __str__(self):
+#         return self.name
+
 
